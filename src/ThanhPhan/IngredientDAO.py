4bde03e8130bc62025a040b1d36fce2a0e9c1851
ThanhTuyen-304 <tuyenc681@gmail.com>
2026-05-27 13:57:38 +0700

    tài khoản và thành phần

--- src/ThanhPhan/IngredientDAO.py
+++ src/ThanhPhan/IngredientDAO.py
@@ -1,6 +1,7 @@
 import pyodbc
 from IngredientDTO import IngredientDTO
 
+
 class IngredientDAO:
     def get_connection(self):
         return pyodbc.connect(
@@ -25,3 +26,16 @@ class IngredientDAO:
 
         conn.close()
         return ingredients
+
+    def get_ingredient_id_by_name(self, ingredient_name):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT ma_thanh_phan FROM ThanhPhan WHERE ten_thanh_phan = ?", (ingredient_name,))
+        row = cursor.fetchone()
+
+        conn.close()
+
+        if row is not None:
+            return row.ma_thanh_phan
+        return None
