fda8e79e914ff032ff7b83e424137d29ddf1fea9
vohuunghia <vohuunghiaah@gmail.com>
2026-06-08 03:27:40 +0700

    update read file and requirements file

--- utils/database_config.py
+++ utils/database_config.py
@@ -2,7 +2,7 @@ import pyodbc
 def database_config():
     return pyodbc.connect(
         "Driver={ODBC Driver 17 for SQL Server};"
-        "Server=DESKTOP-1PJL28F;"  
+        "Server=DESKTOP-J0H3193\\SQLEXPRESS;"  
         "Database=FOOD;" 
         "Trusted_Connection=yes;"
         "TrustServerCertificate=yes;"
